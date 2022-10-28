01dd03779e28a6e20d8af9e441ff3ad60a50928f
MrWrynn <64748923+MrWrynn@users.noreply.github.com>
2022-10-28 15:34:32 -0600

    Add files via upload

--- main.pl
+++ main.pl
@@ -4,12 +4,27 @@ Salida: va llamando las funciones que corresponden a cada una de las preguntas.
 */
 iniciar():-
     write("Bienvenido a Translog"),nl,
-    write("Por Favor indique a que idioma quiere traducir: "),
-    readln(LENG,_,_,_,lowercase), 
-    write(LENG), nl,
+    write("Por Favor indique a que idioma quiere traducir "),
+    write("(Utilice {esp} para espanol y {eng} para ingles) :"),
+    revisarIdioma(LENG),
     write("Muy bien, indique lo que desea traducir: "),
     readln(SENTENCE,_,_,_,lowercase),
     write(SENTENCE),!.
+
+%Revisa que el idioma ingresado por el usuario sea valido
+revisarIdioma(LENG):-
+    read_line_to_string(user_input,S1),
+    string_lower(S1, RESPUESTA),
+    member(RESPUESTA, ["esp", "eng"]),!.
+
+%Cuando la funcion anterior falla, ejecuta esta para pedir nuevamente un idioma al usuario
+revisarIdioma(LENG):-
+    write("Idioma invalido, reingrese su respuesta: "),
+    revisarIdioma(LENG),!.
+
+%BuscarIdioma: busca si el idioma ingresado por el usuario es valido.
+buscarIdioma(RESPUESTA,LENGB):-
+    member(LENGB,RESPUESTA),!.
 
 engtoes(hello, hola).
 engtoes(hi, hola).
