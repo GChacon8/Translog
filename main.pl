25745deae0e79bdb4dfe4e250766249e9f9c539b
GChacon8 <gabriel.chacon.alfaro@gmail.com>
2022-10-29 18:34:50 -0600

    Diccionario avance

--- main.pl
+++ main.pl
@@ -26,6 +26,76 @@ revisarIdioma():-
 buscarIdioma(RESPUESTA,LENGB):-
     member(LENGB,RESPUESTA),!.
 
-engtoes(hello, hola).
-engtoes(hi, hola).
-engtoes("how are you?" ,"Â¿como esta?").
+engtoesp(hello, hola).
+engtoesp(hi, hola).
+engtoesp("how are you?" ,"Â¿como esta?").
+engtoesp(the, los).
+engtoesp(the, las).
+engtoesp(the, el).
+engtoesp(the, la).
+engtoesp(an, un).
+engtoesp(an, una).
+engtoesp(a, un).
+engtoesp(a, una).
+engtoesp(some, unos).
+engtoesp(some, unas).
+
+engtoesp(to, a).
+engtoesp(since, desde).
+engtoesp(against, contra).
+engtoesp(of,de).
+engtoesp(during, durante).
+engtoesp(in, en).
+engtoesp(on, en).
+engtoesp(at, en).
+engtoesp(between, entre).
+engtoesp(towards, hacia).
+engtoesp(until, hasta).
+engtoesp(through, mediante).
+engtoesp(for, para).
+engtoesp(cause, por).
+engtoesp(by, por).
+engtoesp(without, sin).
+engtoesp(on, sobre).
+
+engtoesp(goes, va).
+engtoesp(go ,voy).
+engtoesp(go, vamos).
+engtoesp(go, van).
+engtoesp(am, soy).
+engtoesp(is, es).
+engtoesp(are, somos).
+engtoesp(are, son).
+engtoesp(am, estoy).
+engtoesp(is, está).
+engtoesp(are, estamos).
+engtoesp(are, están).
+engtoesp(have, tengo).
+engtoesp(has, tiene).
+engtoesp(have, tenemos).
+engtoesp(have, tienen).
+engtoesp(talk, hablo).
+engtoesp(talks, habla).
+engtoesp(talk, hablamos).
+engtoesp(talk, hablan).
+engtoesp(say, digo).
+engtoesp(says, dice).
+engtoesp(say, decimos).
+engtoesp(say, dicen).
+engtoesp(feel, siento).
+engtoesp(feels, siente).
+engtoesp(feel, sentimos).
+engtoesp(feel, sienten).
+engtoesp(drink, tomo).
+engtoesp(drinks, toma).
+engtoesp(drink, tomamos).
+engtoesp(drink, toman).
+engtoesp(see, veo).
+engtoesp(sees, ve).
+engtoesp(see, vemos).
+engtoesp(see, ven).
+engtoesp(walk, camino).
+engtoesp(walks, camina).
+engtoesp(walk, caminamos).
+engtoesp(walk, caminan).
+
